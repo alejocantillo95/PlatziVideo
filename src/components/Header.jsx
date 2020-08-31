a870336b7db1f462e2ead7d028bb3e290c5badae
Alejandro Cantillo <jaimecantillo95@gmail.com>
2020-08-31 02:09:16 -0500

    MOD src ADD containers

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <img className="header__img" src="./images/logo-platzi-video-BW2.png" alt="Platzi Video" />
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          <img src="./images/user-icon.png" alt="" />
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li><a href="http://">Cuenta</a></li>
+          <li><a href="http://">Cerrar Sesión</a></li>
+        </ul>
+      </div>
+  </header>
+);
+
+export default Header;
